65aae834dab02db939a7083608f423a8d68ffab7
enricovillagrossi <enrico.villagrossi@stiima.cnr.it>
2021-05-12 15:10:41 +0200

    wip on documentation

--- manipulation_utils/src/manipulation_load_params_utils.cpp
+++ manipulation_utils/src/manipulation_load_params_utils.cpp
@@ -321,7 +321,7 @@ bool InboundPickFromParam::readObjectFromParam()
     {
       listener.lookupTransform("world",frame_name,t0,transform);
     }
-    catch (tf::TransformException ex)
+    catch (tf::TransformException& ex)
     {
       ROS_ERROR("%s",ex.what());
       ros::Duration(1.0).sleep();
