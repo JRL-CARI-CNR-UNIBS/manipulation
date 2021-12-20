ac4b9a100cb2ed7627315675915f2119547a21c6
Manuel Beschi <manuel.beschi@unibs.it>
2021-12-20 15:13:34 +0100

    list location for every server

--- go_to_location/scripts/list_ik_locations.py
+++ go_to_location/scripts/list_ik_locations.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 import rospy
+import rosservice
 from manipulation_msgs.srv import *
 import manipulation_msgs.msg
 import actionlib
 import random
 import time
+import sys
+
+
 if __name__ == "__main__":
     rospy.init_node('list_ik_location')
-    rospy.wait_for_service('/go_to_location_server/list_locations')
 
-    try:
-        list_locations_srv = rospy.ServiceProxy('/go_to_location_server/list_locations', ListOfLocations)
-        resp = list_locations_srv()
-    except rospy.ServiceException as e:
-        print("Service call failed: %s"%e)
+    service_list = rosservice.get_service_list()
+    for service in service_list:
+        if "list_locations" in service:
+            print(service)
+            rospy.wait_for_service(service)
 
-    print(resp.resume)
+            try:
+                list_locations_srv = rospy.ServiceProxy(service, ListOfLocations)
+                resp = list_locations_srv()
+            except rospy.ServiceException as e:
+                print("Service call failed: %s"%e)
+
+            print(resp.resume)
